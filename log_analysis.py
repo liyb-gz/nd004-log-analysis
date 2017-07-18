c77167e33d8d0026bc03ea846b6abcc2b9015d82
Felix Li <liyb.gz@gmail.com>
2017-07-18 18:37:43 +0800

    Reconstruct each analysis into a function

--- log_analysis.py
+++ log_analysis.py
@@ -12,14 +12,37 @@ order by num_of_view desc
 limit 3;
 '''
 
+analysis_list = [
+	{
+		'question': '1. What are the most popular three articles of all time?',
+		'query':  '''
+select title, sum(path_count) as num_of_view 
+from articles, path_log 
+where path like ('%'||slug||'%')
+group by title
+order by num_of_view desc
+limit 3;
+''',
+		'answer_template': '\t"%s" : %d views'
+	}
+]
+
+def run_analysis(cur, analysis):
+	cur.execute(analysis['query'])
+	rows = cur.fetchall()
+
+	print(analysis['question'])
+
+	for row in rows:
+		print(analysis['answer_template'] % row)
+
 def log_analysis():
 	''' Run log analysis '''
-	conn = psycopg2.connect("dbname=%s" % DBNAME)
+	conn = psycopg2.connect('dbname=%s' % DBNAME)
 	cur = conn.cursor()
-	cur.execute(QUERY)
-	rows = cur.fetchall()
-	for row in rows:
-		print("%s : %d" % row)
+
+	for analysis in analysis_list:
+		run_analysis(cur, analysis)
 
 	conn.close()
 
