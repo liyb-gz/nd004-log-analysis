4aae3246d3552397a1549d0bf2d35dea30ae4755
Felix Li <liyb.gz@gmail.com>
2017-07-18 18:05:08 +0800

    Add the first query

--- log_analysis.py
+++ log_analysis.py
@@ -3,5 +3,17 @@ import psycopg2
 # Constants
 DBNAME = "news"
 
+QUERY = '''
+select title, sum(path_count) as num_of_view 
+from articles, path_log 
+where path like ('%'||slug||'%')
+group by title
+order by num_of_view desc
+limit 3;
+'''
+
 conn = psycopg2.connect("dbname=%s" % DBNAME)
+cur = conn.cursor()
+cur.execute(QUERY)
+print(cur.fetchall())
 conn.close()
