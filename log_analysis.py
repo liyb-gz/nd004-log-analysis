696f1895e0734e0f1e40558f83cddb60a3359992
Felix Li <liyb.gz@gmail.com>
2017-07-18 18:46:30 +0800

    add the second question

--- log_analysis.py
+++ log_analysis.py
@@ -3,27 +3,29 @@ import psycopg2
 # Constants
 DBNAME = "news"
 
-QUERY = '''
-select title, sum(path_count) as num_of_view 
-from articles, path_log 
-where path like ('%'||slug||'%')
-group by title
-order by num_of_view desc
-limit 3;
-'''
-
 analysis_list = [
 	{
 		'question': '1. What are the most popular three articles of all time?',
 		'query':  '''
 select title, sum(path_count) as num_of_view 
-from articles, path_log 
+from article_slug_author, path_log 
 where path like ('%'||slug||'%')
 group by title
 order by num_of_view desc
 limit 3;
 ''',
 		'answer_template': '\t"%s" : %d views'
+	},
+	{
+		'question': '2. Who are the most popular article authors of all time?',
+		'query':  '''
+select name, sum(path_count) as num_of_view 
+from article_slug_author, path_log 
+where path like ('%'||slug||'%')
+group by name
+order by num_of_view desc;
+''',
+		'answer_template': '\t%s : %d views'
 	}
 ]
 
@@ -36,8 +38,10 @@ def run_analysis(cur, analysis):
 	for row in rows:
 		print(analysis['answer_template'] % row)
 
+	print()
+
 def log_analysis():
-	''' Run log analysis '''
+	''' Start the whole log analysis '''
 	conn = psycopg2.connect('dbname=%s' % DBNAME)
 	cur = conn.cursor()
 
